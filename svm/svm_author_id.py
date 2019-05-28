65b3f6187c2f1cfc3653c676b2214b0ba7eb0129
Loucher <matus.svitek@gmail.com>
2019-05-28 22:52:35 +0200

    Finished NB and SVM chapters

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -7,24 +7,30 @@
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
+
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+from collections import Counter
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
-
-
-
-
+# features_train = features_train[:len(features_train) / 100]
+# labels_train = labels_train[:len(labels_train) / 100]
 #########################################################
-### your code goes here ###
-
+clf = SVC(kernel='rbf', C=10000)
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time() - t0, 3), "s"
+t0 = time()
+pred = clf.predict(features_test)
+print "predicting time:", round(time() - t0, 3), "s"
+print "accuracy: ", round(accuracy_score(pred, labels_test), 2)
+print Counter(pred)
 #########################################################
-
-
